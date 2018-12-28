3890a784cec0256b15192fd1465818b6a64ecaa0
123 <123>
2018-12-28 15:51:32 +0800

    2

--- mcfwq/bm/opencode/getcodeurl.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$pageid=$_GET['ID'];
-header("Content-type: text/html; charset=utf-8");  
-$appid = "wx863e78b1aaf4d6f4";  
-$url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='.$appid.'&redirect_uri=http://ym.2dreamchaser.com/yin/2018/bm/opencode/index.php&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';  
-header("Location:".$url); 
-?>
--- mcfwq/hongbao/hb0330/db.php
+++
@@ -1,6 +0,0 @@
-<?php 
-	mysql_connect('47.98.42.19','root','MengChang@1027!');//
-	mysql_select_db('yin');
-	mysql_query('set names utf8');
-	$tbname='hb0330';
-?>
